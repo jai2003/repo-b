f4fc5c8c8547b300bd767b927e3a115bc13e5830
jai2003 <91177325+jai2003@users.noreply.github.com>
2025-01-29 12:31:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 const _ = require('repo-a'); // Import Repo A
-console.log("Repo B is usksngs;ing Repo A hel;klkndvkowejnfowfdlawnflo world", _.VERSION); // Just an example of using a package from Repo A
+console.log("Repo B is usksngs;ing Repo A hel;klkndvkowejnfowfiomfpriemf[erdlawnflo world", _.VERSION); // Just an example of using a package from Repo A
